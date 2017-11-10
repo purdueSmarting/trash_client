6f578aff00c7843a46e4039302bd59e5cfa8e6f9
SuYunPark <spark5@nate.com\]>
2017-11-09 19:38:18 -0500

    to json

--- trash.py
+++ trash.py
@@ -10,19 +10,21 @@ import httplib as http
 import requests
 import serial
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=2)
 ser.open
 
 total_height = 68.0
-while 1:
-    current_height = ser.readline()
-    current_time = datetime.datetime.now()
+while True:
+    print "loop start"
+    current_time = str(datetime.datetime.now())
+    current_height = int(ser.readline())
 
     #How much is trash can filled
     percentage = (total_height - float(current_height)) / total_height *100 
-    
-    data = "Time:"+ str(current_time) + "Distance from trash: " + str(current_height) + "Percentage: " + str(int(percentage)) + "%"
-    data = json.loads(json.dumps(data))
-    print data
-    r = requests.post('http://192.168.2.136:7579/trash', json={'data':data})
+
+    payload = {'time':current_time,'current_height':current_height,'percentage':percentage}
+    jsonString = json.dumps(payload)
+    print (jsonString)    
+    requests.post('http://192.168.2.136:1235/trash', data=payload)
+    print "request posted"
     time.sleep(2)
